13150949a1833fb676be05d5a4a137297168a489
Ster Roque <79429966+SterRoque@users.noreply.github.com>
2025-01-06 19:23:04 -0400

    feat: login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,43 @@
+import { Input } from '@/src/components/Input';
+import { PackageOpen } from 'lucide-react';
+import Link from 'next/link';
+
 export default function Home() {
-  return (
-    <div>
-      <h1>ola loguinos</h1>
-    </div>
-  );
+   return (
+      <main className='flex justify-center items-center h-screen bg-blue-50'>
+         <form className='flex flex-col h-fit max-w-96 w-full border rounded-xl p-14 bg-white'>
+            <div className='flex gap-2 justify-center items-center'>
+               <PackageOpen />
+               <h1 className='text-xl font-semibold'>Loguinos</h1>
+            </div>
+            <div className='flex flex-col gap-3 w-full mt-10'>
+               <Input
+                  placeholder='Email'
+                  type='email'
+               />
+               <Input
+                  placeholder='Senha'
+                  type='password'
+               />
+               <Link
+                  href={''}
+                  className='flex self-end text-xs text-blue-600 font-semibold'>
+                  Esqueceu sua senha?
+               </Link>
+            </div>
+            <button className='bg-blue-600 text-white rounded-lg h-9 mt-7 text-sm'>
+               Entrar
+            </button>
+
+            <p className='text-xs text-center mt-7'>
+               Ainda não tem uma conta?{' '}
+               <Link
+                  href={''}
+                  className='text-blue-600'>
+                  Cadastre-se
+               </Link>
+            </p>
+         </form>
+      </main>
+   );
 }
