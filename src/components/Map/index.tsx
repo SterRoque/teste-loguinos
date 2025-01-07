03eab36a6cf44304dfd22c1b6ed55e7734cdf7e5
Ster Roque <79429966+SterRoque@users.noreply.github.com>
2025-01-07 00:21:21 -0400

    feat: map component

---
+++ src/components/Map/index.tsx
@@ -0,0 +1,31 @@
+'use client';
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const Map = () => {
+   useEffect(() => {
+      const map = L.map('map').setView([-23.55052, -46.633308], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         attribution:
+            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      const marker = L.marker([-23.55052, -46.633308]).addTo(map);
+      marker.bindPopup('<b>São Paulo</b><br>Este é um exemplo!').openPopup();
+
+      return () => {
+         map.remove();
+      };
+   }, []);
+
+   return (
+      <div
+         id='map'
+         className='h-[350px] w-full'
+      />
+   );
+};
+
+export default Map;
