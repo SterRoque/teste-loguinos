95604ab9582b7f33e6e58e75bfb5a3fb2833e668
Ster Roque <79429966+SterRoque@users.noreply.github.com>
2025-01-06 23:39:53 -0400

    feat: implement sidebar in maps

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -1,9 +1,18 @@
+'use client';
+
 import { Header } from '@/src/components/Header';
+import { Sidebar } from '@/src/components/Sidebar';
+import { cn } from '@/src/utils/cn';
+import { useState } from 'react';
 
 export default function Maps() {
    return (
-      <div className='bg-gray-100 h-screen'>
-         <Header />
+      <div className='bg-gray-100 h-screen flex'>
+         <Sidebar />
+         <div className='w-full'>
+            <Header />
+            <main>conteudoooooooo</main>
+         </div>
       </div>
    );
 }
