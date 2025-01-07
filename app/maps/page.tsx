1a7dcdb0decab66c663605592589b30028f0c5c2
Ster Roque <79429966+SterRoque@users.noreply.github.com>
2025-01-06 20:47:20 -0400

    feat: maps page

---
+++ app/maps/page.tsx
@@ -0,0 +1,9 @@
+import { Header } from '@/src/components/Header';
+
+export default function Maps() {
+   return (
+      <div className='bg-gray-100 h-screen'>
+         <Header />
+      </div>
+   );
+}
