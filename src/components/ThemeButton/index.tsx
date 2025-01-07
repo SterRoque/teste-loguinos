b778a0252271fc156d6d3fe1f5d808f470389eee
SterRoque <stermiss2@gmail.com>
2025-01-07 07:03:25 -0300

    feat: theme button component

---
+++ src/components/ThemeButton/index.tsx
@@ -0,0 +1,29 @@
+import { Moon, Sun } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
+export function ThemeButton() {
+   const [darkMode, setDarkMode] = useState(() => {
+      return localStorage.getItem('theme') === 'dark';
+   });
+
+   useEffect(() => {
+      if (darkMode) {
+         document.documentElement.classList.add('dark');
+         localStorage.setItem('theme', 'dark');
+      } else {
+         document.documentElement.classList.remove('dark');
+         localStorage.setItem('theme', 'light');
+      }
+   }, [darkMode]);
+
+   function handleToggleMode() {
+      setDarkMode((prev) => !prev);
+   }
+   return (
+      <button
+         className='w-8 h-8 rounded-full bg-gray-800 text-white flex justify-center items-center'
+         onClick={handleToggleMode}>
+         {darkMode ? <Moon size={18} /> : <Sun size={18} />}
+      </button>
+   );
+}
