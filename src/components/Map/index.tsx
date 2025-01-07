373e4b176c1a1a010107e35a9c213132eb26b1ed
Ster Roque <79429966+SterRoque@users.noreply.github.com>
2025-01-07 00:23:23 -0400

    fix: ajust name function

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const Map = () => {
+export function Map() {
    useEffect(() => {
       const map = L.map('map').setView([-23.55052, -46.633308], 13);
 
@@ -26,6 +26,4 @@ const Map = () => {
          className='h-[350px] w-full'
       />
    );
-};
-
-export default Map;
+}
