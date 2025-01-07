5e76a838a8b5526f6f2ab0ef2319e9942b5aa024
Ster Roque <79429966+SterRoque@users.noreply.github.com>
2025-01-06 23:38:48 -0400

    fix: changing the header width

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { Input } from '../Input';
 
 export function Header() {
    return (
-      <header className='h-14 bg-slate-900 w-screen flex items-center justify-between px-4'>
+      <header className='h-14 bg-slate-900 w-full flex items-center justify-between px-4 '>
          <Input
             className='w-56 h-8'
             placeholder='Pesquisar'
