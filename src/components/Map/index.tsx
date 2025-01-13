8ec357ee514fa86bea067222de8630a5185529b4
Sterphane Roque <stermiss2@gmail.com>
2025-01-13 12:32:00 -0300

    fix: maps

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,31 +1,30 @@
 'use client';
-
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import 'leaflet/dist/leaflet.css';
 import { renderToString } from 'react-dom/server';
 import { PopupCustom } from '../PopupCustom';
 import dynamic from 'next/dynamic';
 import { IOrder } from '@/src/interfaces/order-interface';
-
+import L from 'leaflet';
 type MapProps = {
    orders: IOrder[];
 };
-
 export function Map({ orders }: MapProps) {
+   const mapRef = useRef<L.Map | null>(null);
    useEffect(() => {
       if (typeof window === 'undefined') return;
-
+      if (mapRef.current) {
+         return;
+      }
       import('leaflet').then((L) => {
          const map = L.map('map').setView(
             [orders[0].latitude, orders[0].longitude],
             13,
          );
-
          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution:
                '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
          }).addTo(map);
-
          orders.forEach((order) => {
             const marker = L.marker([order.latitude, order.longitude]).addTo(
                map,
@@ -33,13 +32,13 @@ export function Map({ orders }: MapProps) {
             const popup = renderToString(<PopupCustom order={order} />);
             marker.bindPopup(popup);
          });
-
+         mapRef.current = map;
          return () => {
             map.remove();
+            mapRef.current = null;
          };
       });
    }, [orders]);
-
    return (
       <div
          id='map'
@@ -47,5 +46,4 @@ export function Map({ orders }: MapProps) {
       />
    );
 }
-
 export const DynamicMap = dynamic(() => Promise.resolve(Map), { ssr: false });
